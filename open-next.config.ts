041767060c7c123825a10195623f5596d8526fbd
Radu Luncasu <radu.luncasu@gmail.com>
2025-02-27 18:30:01 +0200

    attempt fix cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,7 @@
+import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
 import cache from "@opennextjs/cloudflare/kvCache";
 
-const config = {
+const config: OpenNextConfig = {
   default: {
     override: {
       wrapper: "cloudflare-node",
@@ -11,14 +12,14 @@ const config = {
     },
   },
 
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
+  // middleware: {
+  //   external: true,
+  //   override: {
+  //     wrapper: "cloudflare-edge",
+  //     converter: "edge",
+  //     proxyExternalRequest: "fetch",
+  //   },
+  // },
 
   dangerous: {
     enableCacheInterception: false,
